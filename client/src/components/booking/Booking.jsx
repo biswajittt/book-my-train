8a9725ee58a7bcdd597825141fa51ae4b7fbabb5
biswajittt <biswajitdeb324@gmail.com>
2025-03-27 16:33:34 +0530

    bug fixed

--- client/src/components/booking/Booking.jsx
+++ client/src/components/booking/Booking.jsx
@@ -322,30 +322,34 @@ export default function Booking() {
                             {traveller.age}
                           </div>
                         </div>
-                        <button
-                          type="button"
-                          class="inline-flex items-center text-sm font-medium text-red-600 hover:underline dark:text-red-500"
-                          onClick={() => removeTravellerField(index)}
-                        >
-                          <svg
-                            class="me-1.5 h-5 w-5"
-                            aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="24"
-                            height="24"
-                            fill="none"
-                            viewBox="0 0 24 24"
+                        {loading ? (
+                          "Loading..."
+                        ) : (
+                          <button
+                            type="button"
+                            class="inline-flex items-center text-sm font-medium text-red-600 hover:underline dark:text-red-500"
+                            onClick={() => removeTravellerField(index)}
                           >
-                            <path
-                              stroke="currentColor"
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
-                              stroke-width="2"
-                              d="M6 18 17.94 6M18 18 6.06 6"
-                            />
-                          </svg>
-                          Remove
-                        </button>
+                            <svg
+                              class="me-1.5 h-5 w-5"
+                              aria-hidden="true"
+                              xmlns="http://www.w3.org/2000/svg"
+                              width="24"
+                              height="24"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                            >
+                              <path
+                                stroke="currentColor"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M6 18 17.94 6M18 18 6.06 6"
+                              />
+                            </svg>
+                            Remove
+                          </button>
+                        )}
                       </div>
                     ))
                   ) : (
