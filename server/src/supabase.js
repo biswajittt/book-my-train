be702cda1844503f8c73fe6be1e76a6c6dd67f01
biswajittt <biswajitdeb324@gmail.com>
2025-03-27 11:10:11 +0530

    all features added

---
+++ server/src/supabase.js
@@ -0,0 +1,10 @@
+import { createClient } from "@supabase/supabase-js";
+import dotenv from "dotenv";
+dotenv.config();
+
+const SUPABASE_URL = process.env.SUPABASE_URL;
+const SUPABASE_SERVICE_ROLE_KEY = process.env.SUPABASE_ANON_KEY; // Secure this in environment variables
+
+const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY);
+
+export default supabase;
