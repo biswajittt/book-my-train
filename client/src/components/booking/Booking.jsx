a27a53dd8f5bbf296ae9562dcabf3253f182f731
biswajittt <biswajitdeb324@gmail.com>
2025-03-27 13:12:47 +0530

    bug fixed

--- client/src/components/booking/Booking.jsx
+++ client/src/components/booking/Booking.jsx
@@ -183,7 +183,7 @@ export default function Booking() {
           class="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
         >
           <div class="m-auto top-[22%] relative p-4 w-auto sm:w-2xl  max-h-full">
-            <div class="relative bg-white rounded-lg shadow-sm dark:bg-gray-700">
+            <div class="relative rounded-lg shadow-sm bg-gray-700">
               <button
                 type="button"
                 class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
